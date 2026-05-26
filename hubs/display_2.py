34d8c9789c1116e475ef97d8fd2ae759a6bc3ce6
Martinnygaard <mm.nygaard@gmail.com>
2026-05-26 10:44:21 +0200

    bisect-14: fix Color API (Color.BLUE not 'blue')

--- hubs/display_2.py
+++ hubs/display_2.py
@@ -1,9 +1,11 @@
-# BISECT-13: visuel LED-test (ingen stdout dependence)
+# BISECT-14: visuel LED-test med korrekt Color-API
 from pybricks.hubs import PrimeHub
+from pybricks.parameters import Color
 from pybricks.tools import wait
 hub = PrimeHub()
-hub.light.on("blue")
+hub.light.on(Color.BLUE)
 wait(2000)
-hub.light.on("yellow")
+hub.light.on(Color.YELLOW)
 wait(2000)
 hub.light.off()
+print("BISECT14_DONE")
